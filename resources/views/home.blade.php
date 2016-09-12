c79f5438620bba0caa4b9c073d1ff895cb4b58dd
Cleverson <cleverson.s.dias@gmail.com>
2016-09-12 15:17:10 -0300

    Alterado home.blade.php_3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Painel de Controle</div>
+                <div class="panel-heading">Painel de Administrativo</div>
 
                 <div class="panel-body">
                     <h2>Você está logado!</h2>
